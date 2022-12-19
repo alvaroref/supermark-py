7c5751d3184342016eae613c084b6403ecbcec0f
PerezNatael <91854424+PerezNatael@users.noreply.github.com>
2022-12-19 20:10:39 -0300

    Update main.py
    estaba importando de un archivo que ya no existe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from Inicio_sesion import Inicio_sesion
-from Principal import vista_principal
 import tkinter as tk
 import tkinter.font as tkFont
-
 
 if __name__ == "__main__":
     root = tk.Tk()
